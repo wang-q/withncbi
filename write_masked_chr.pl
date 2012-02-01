680def35b825bafd7a3133c031868d882868dead
wangq <wangqiang1997@gmail.com>
2012-02-02 01:07:30 +0800

    Combine all non-chromosome contigs

--- write_masked_chr.pl
+++ write_masked_chr.pl
@@ -16,6 +16,7 @@ use File::Basename;
 use String::Compare;
 use List::Util qw(reduce);
 use List::MoreUtils qw(zip);
+use Set::Scalar;
 
 use AlignDB::IntSpan;
 use AlignDB::Stopwatch;
@@ -66,7 +67,7 @@ pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
 # Init objects
 #----------------------------------------------------------#
 my $stopwatch = AlignDB::Stopwatch->new;
-$stopwatch->start_message("Write masked chr from $ensembl_db...");
+$stopwatch->start_message("Write masked chr...");
 
 #----------------------------#
 # Get runlist of $feature
@@ -120,7 +121,7 @@ else {
 if ($dir_fa) {
     my @files = sort File::Find::Rule->file->name('*.fa')->in($dir_fa);
 
-    my @chrs = map { basename $_ , ".fa" } @files; # strip dir and suffix
+    my @chrs = map { basename $_ , ".fa" } @files;    # strip dir and suffix
     while (1) {
         my $lcss = lcss(@chrs);
         last unless $lcss;
@@ -130,35 +131,56 @@ if ($dir_fa) {
     }
     my $file_of = { zip( @chrs, @files ) };
 
-    for my $file_chr ( sort @chrs ) {
+    my @matched_files;
+    for my $ftr_chr ( keys %{$ftr_of} ) {
 
-        # use the most similar chr name
-        my ($ftr_chr) = map { $_->[0] }
+        # use the most similar file name
+        my ($file_chr) = map { $_->[0] }
             sort { $b->[1] <=> $a->[1] }
-            map { [ $_, compare( $_, $file_chr ) ] } keys %{$ftr_of};
+            map { [ $_, compare( $_, $ftr_chr ) ] } keys %{$file_of};
 
         printf "Write masked seq for ftr_chr %8s file_chr %8s\n", $ftr_chr,
             $file_chr;
 
+        # feature set
         my $ftr_set = AlignDB::IntSpan->new( $ftr_of->{$ftr_chr} );
 
+        # seq
         my ( $seq_of, $seq_names ) = read_fasta( $file_of->{$file_chr} );
         my $seq = $seq_of->{ $seq_names->[0] };
-
+        
         my @sets = $ftr_set->sets;
         for my $set (@sets) {
             my $offset = $set->min - 1;
             my $length = $set->size;
-
+        
             my $str = substr $seq, $offset, $length;
             $str = lc $str;
             substr $seq, $offset, $length, $str;
         }
-
+        
         open my $out_fh, '>', $file_of->{$file_chr} . ".masked";
         print {$out_fh} ">chr$ftr_chr\n";
         print {$out_fh} substr( $seq, 0, $linelen, '' ) . "\n" while ($seq);
         close $out_fh;
+
+        push @matched_files, $file_of->{$file_chr};
+    }
+
+    {    # combine all unmatched filess to chrUn.fasta
+        my $fa_set = Set::Scalar->new;
+        $fa_set->insert($_) for @files;
+        $fa_set->delete($_) for @matched_files;
+
+        printf "There are %d unmatched file\n", $fa_set->size;
+        if ( $fa_set->size ) {
+            my $str = join " ", map { basename $_} $fa_set->elements;
+            print "We'll combine these files into chrUn.fasta\n";
+            print $str, "\n";
+
+            system "cat $str > chrUn.fasta";
+            system "rm $str";
+        }
     }
 }
 
