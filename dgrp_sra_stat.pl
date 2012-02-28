7ae001227d3abf63244562309af481164622a93a
wangq <wangqiang1997@gmail.com>
2012-02-28 08:28:25 +0800

    Add cmdline options

--- dgrp_sra_stat.pl
+++ dgrp_sra_stat.pl
@@ -8,14 +8,42 @@ use Pod::Usage;
 use YAML qw(Dump Load DumpFile LoadFile);
 
 use Text::CSV_XS;
+use File::Basename;
 
-my $yml = LoadFile("DGRP.yml");
+#----------------------------------------------------------#
+# GetOpt section
+#----------------------------------------------------------#
+my $yml_file;
+my $platform_rx = "Illumina";
+my $layout_rx   = "pair";
+
+my $man  = 0;
+my $help = 0;
+
+GetOptions(
+    'help|?'       => \$help,
+    'man'          => \$man,
+    'y|yml=s'      => \$yml_file,
+    'p|platform=s' => \$platform_rx,
+    'l|layout=s'   => \$layout_rx,
+) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+
+#----------------------------------------------------------#
+# start
+#----------------------------------------------------------#
+my $yml      = LoadFile($yml_file);
+my $basename = basename($yml_file) . ".$platform_rx" . ".$layout_rx";
 
 my $csv = Text::CSV_XS->new( { binary => 1 } )
-    or die "Cannot use CSV: " . Text::CSV_XS->error_diag();
+    or die "Cannot use CSV: " . Text::CSV_XS->error_diag;
 $csv->eol("\n");
-open my $out_fh, ">", "dgrp.csv";
-open my $ftp_fh, ">", "dgrp.ftp.txt";
+
+open my $out_fh, ">", "$basename.csv";
+open my $ftp_fh, ">", "$basename.ftp.txt";
+
 for my $name ( sort keys %{$yml} ) {
     print "$name\n";
 
@@ -29,8 +57,8 @@ for my $name ( sort keys %{$yml} ) {
         my $layout = $info->{layout};
         print " " x 8, $layout, "\n";
 
-        next unless $platform =~ /illumina|solexa/i;
-        next unless $layout   =~ /pair/i;
+        next unless $platform =~ qr/$platform_rx/i;
+        next unless $layout   =~ qr/$layout_rx/i;
 
         for my $i ( 0 .. scalar @{ $info->{srr} } - 1 ) {
             my $srr = $info->{srr}[$i];
@@ -48,5 +76,23 @@ for my $name ( sort keys %{$yml} ) {
         }
     }
 }
+
 close $ftp_fh;
 close $out_fh;
+
+__END__
+
+=head1 NAME
+
+    sra_stat.pl - stats for sra
+
+=head1 SYNOPSIS
+    perl sra_stat.pl --in_dir G:/S288CvsRM11 --msa muscle --quick
+
+    sra_stat.pl [options]
+      Options:
+        --help              brief help message
+        --man               full documentation
+        -y, --yml           yaml file of sra info
+        -p, --platform      illumina or 454
+        -l, --layout        pair or single
