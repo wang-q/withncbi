23df7f019bed2ae16046161862cc52097951deb0
wangq <wangqiang1997@gmail.com>
2014-04-26 13:35:21 +0800

    There're barely no history for there files. So just copy them here.

---
+++ db/gr_db.pl
@@ -0,0 +1,240 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+
+use Getopt::Long;
+use Pod::Usage;
+use Config::Tiny;
+use YAML qw(Dump Load DumpFile LoadFile);
+
+use DBI;
+use Text::CSV_XS;
+use File::Slurp;
+use List::MoreUtils qw(any all uniq);
+
+use FindBin;
+
+use AlignDB::Stopwatch;
+
+#----------------------------------------------------------#
+# GetOpt section
+#----------------------------------------------------------#
+my $Config = Config::Tiny->new;
+$Config = Config::Tiny->read("$FindBin::Bin/../config.ini");
+
+# record ARGV and Config
+my $stopwatch = AlignDB::Stopwatch->new(
+    program_name => $0,
+    program_argv => [@ARGV],
+    program_conf => $Config,
+);
+
+# Database init values
+my $server   = $Config->{database}{server};
+my $port     = $Config->{database}{port};
+my $username = $Config->{database}{username};
+my $password = $Config->{database}{password};
+my $db_name  = $Config->{database}{db};
+
+my $init_sql = "$FindBin::Bin/../init.sql";
+
+# running options
+my $strain_file = "prok_strains.csv";
+my $gr_dir      = $Config->{path}{gr};
+
+# append euk
+my $append;
+
+my $man  = 0;
+my $help = 0;
+
+GetOptions(
+    'help|?'       => \$help,
+    'man'          => \$man,
+    's|server=s'   => \$server,
+    'P|port=i'     => \$port,
+    'u|username=s' => \$username,
+    'p|password=s' => \$password,
+    'd|db=s'       => \$db_name,
+    'init_sql=s'   => \$init_sql,
+    'file=s'       => \$strain_file,
+    'append=s'     => \$append,
+) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+
+#----------------------------------------------------------#
+# init
+#----------------------------------------------------------#
+$stopwatch->start_message("Init genome report DB...");
+
+#----------------------------#
+# call mysql
+#----------------------------#
+if ( !$append ) {
+    $stopwatch->block_message("Create DB skeleton for $db_name");
+
+    my $drh = DBI->install_driver("mysql");    # Driver handle object
+    $drh->func( 'dropdb',   $db_name, $server, $username, $password, 'admin' );
+    $drh->func( 'createdb', $db_name, $server, $username, $password, 'admin' );
+    $drh->func( 'reload',   $db_name, $server, $username, $password, 'admin' );
+
+    my $dbh
+        = DBI->connect( "dbi:mysql:$db_name:$server", $username, $password );
+    open my $infh, '<', $init_sql;
+    my $content = do { local $/; <$infh> };
+    close $infh;
+    my @statements = grep {/\w/} split /;/, $content;
+    for (@statements) {
+        $dbh->do($_) or die $dbh->errstr;
+    }
+}
+
+my $dbh = DBI->connect( "dbi:mysql:$db_name:$server", $username, $password );
+
+#----------------------------#
+# Filling table strain
+#----------------------------#
+{
+    $stopwatch->block_message("Loading $strain_file");
+
+    my $load_sth = $dbh->prepare(
+        qq{
+        LOAD DATA LOCAL INFILE '$strain_file'
+        INTO TABLE gr
+        FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n'
+        IGNORE 1 LINES
+        }
+    );
+    $load_sth->execute;
+    $load_sth->finish;
+}
+
+#----------------------------#
+# Add code from GENOME_REPORTS
+#----------------------------#
+if ( !$append ) {
+    $stopwatch->block_message("Add code");
+
+    my @references;
+    for my $file (
+        "$gr_dir/prok_reference_genomes.txt",
+        "$gr_dir/prok_representative_genomes.txt"
+        )
+    {
+        my @lines = read_file( $file, chomp => 1, );
+        push @references, @lines;
+    }
+
+    my %code_of;
+    for my $line ( uniq(@references) ) {
+        my ( $name, $code ) = ( split /\t/, $line )[ 2, 7 ];
+        if ( exists $code_of{$name} ) {
+            $code_of{$name} .= "/$code";
+        }
+        else {
+            $code_of{$name} = $code;
+        }
+    }
+
+    my $update_sth = $dbh->prepare(
+        qq{
+        UPDATE  gr
+        SET     code = ?
+        WHERE   organism_name = ?
+        }
+    );
+    for my $name ( sort keys %code_of ) {
+        $update_sth->execute( $code_of{$name}, $name );
+    }
+    $update_sth->finish;
+}
+
+#----------------------------#
+# Count members
+#----------------------------#
+{
+    $stopwatch->block_message("Update species and genus memberships");
+
+    # find species contains multiply strains
+    my %species_member_of;
+    my $species_sth = $dbh->prepare(
+        qq{
+        SELECT species, count(taxonomy_id)
+        FROM   gr
+        WHERE 1 = 1
+        GROUP BY species
+        }
+    );
+    $species_sth->execute;
+    while ( my @row = $species_sth->fetchrow_array ) {
+        my ( $species, $count ) = @row;
+        $species_member_of{$species} = $count;
+    }
+    $species_sth->finish;
+
+    # find genus contains multiply species
+    my %genus_member_of;
+    my $genus_sth = $dbh->prepare(
+        qq{
+        SELECT genus, count(distinct species), count(taxonomy_id)
+        FROM   gr
+        WHERE 1 = 1
+        GROUP BY genus
+        }
+    );
+    $genus_sth->execute;
+    while ( my @row = $genus_sth->fetchrow_array ) {
+        my ( $genus, $species_count, $strain_count ) = @row;
+        $genus_member_of{$genus} = [ $species_count, $strain_count ];
+    }
+    $genus_sth->finish;
+
+    my $update_sth = $dbh->prepare(
+        qq{
+        UPDATE  gr
+        SET     species_member = ?,
+                genus_species_member = ?,
+                genus_strain_member = ?
+        WHERE   taxonomy_id = ?
+        }
+    );
+    my $id_sth = $dbh->prepare(
+        qq{
+        SELECT taxonomy_id, species, genus
+        FROM   gr
+        }
+    );
+    $id_sth->execute;
+    while ( my @row = $id_sth->fetchrow_array ) {
+        my ( $taxonomy_id, $species, $genus ) = @row;
+        $update_sth->execute(
+            $species_member_of{$species},  $genus_member_of{$genus}->[0],
+            $genus_member_of{$genus}->[1], $taxonomy_id
+        );
+    }
+    $id_sth->finish;
+    $update_sth->finish;
+}
+
+#----------------------------#
+# Finish
+#----------------------------#
+$stopwatch->end_message;
+exit;
+
+__END__
+
+=head1 NAME
+
+gr_db.pl
+
+=head1 SYNOPSIS
+
+perl gr_db.pl --file prok_strains.csv
+
+perl gr_db.pl --append --file euk_strains.csv
+
+=cut
---
+++ db/gr_strains.pl
@@ -0,0 +1,272 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+
+use Getopt::Long;
+use Pod::Usage;
+use Config::Tiny;
+use YAML qw(Dump Load DumpFile LoadFile);
+
+use DBI;
+use Text::CSV_XS;
+use Bio::Taxon;
+use Bio::DB::Taxonomy;
+use DateTime::Format::Natural;
+use List::MoreUtils qw(any all uniq);
+
+use AlignDB::Stopwatch;
+
+use FindBin;
+
+#----------------------------------------------------------#
+# GetOpt section
+#----------------------------------------------------------#
+my $Config = Config::Tiny->new;
+$Config = Config::Tiny->read("$FindBin::Bin/../config.ini");
+
+# record ARGV and Config
+my $stopwatch = AlignDB::Stopwatch->new(
+    program_name => $0,
+    program_argv => [@ARGV],
+    program_conf => $Config,
+);
+
+# running options
+my $gr_dir = $Config->{path}{gr};
+my $bp_dir = $Config->{path}{bp};
+my $td_dir = $Config->{path}{td};
+
+# eukaryotes instead of prokaryotes
+my $euk;
+
+my $strain_file = "prok_strains.csv";
+
+my $man  = 0;
+my $help = 0;
+
+GetOptions(
+    'euk'        => \$euk,
+    'o|output=s' => \$strain_file,
+    'help|?'     => \$help,
+    'man'        => \$man,
+) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+
+#----------------------------------------------------------#
+# init
+#----------------------------------------------------------#
+$stopwatch->start_message("Writing strains' summary...");
+
+$stopwatch->block_message("Load ncbi taxdmp");
+my $taxon_db = Bio::DB::Taxonomy->new(
+    -source    => 'flatfile',
+    -directory => $td_dir,
+    -nodesfile => "$td_dir/nodes.dmp",
+    -namesfile => "$td_dir/names.dmp",
+);
+
+#----------------------------#
+# load tab sep. txt files
+#----------------------------#
+$stopwatch->block_message("Load ncbi genome report and bioproject summary");
+my $dbh = DBI->connect("DBI:CSV:");
+
+if ( !$euk ) {
+
+    # Chromosomes_RefSeq ==> Chromosomes
+    $dbh->{csv_tables}->{t0} = {
+        eol            => "\n",
+        sep_char       => "\t",
+        file           => "$gr_dir/prokaryotes.txt",
+        skip_first_row => 1,
+        quote_char     => '',
+        col_names      => [
+            qw{ Organism_Name TaxID BioProject_Accession BioProject_ID Group
+                SubGroup Size GC Chromosomes Chromosomes_INSDC Plasmids_RefSeq
+                Plasmids_INSDC WGS Scaffolds Genes Proteins Release_Date Modify_Date
+                Status Center }
+        ],
+    };
+}
+else {
+    $dbh->{csv_tables}->{t0} = {
+        eol            => "\n",
+        sep_char       => "\t",
+        file           => "$gr_dir/eukaryotes.txt",
+        skip_first_row => 1,
+        quote_char     => '',
+        col_names      => [
+            qw{ Organism_Name TaxID BioProject_Accession BioProject_ID Group
+                SubGroup Size GC Assembly Chromosomes Organelles Plasmids WGS
+                Scaffolds Genes Proteins Release_Date Modify_Date Status Center }
+        ],
+    };
+}
+
+$dbh->{csv_tables}->{t1} = {
+    eol            => "\n",
+    sep_char       => "\t",
+    file           => "$bp_dir/summary.txt",
+    skip_first_row => 1,
+    quote_char     => '',
+    col_names      => [
+        qw{ Organism_Name TaxID Project_Accession Project_ID Project_Type
+            Project_Data_Type Date }
+    ],
+};
+
+#----------------------------#
+# join t0 and t1
+#----------------------------#
+{
+    $stopwatch->block_message("Write summary");
+
+    my $query = qq{
+        SELECT 
+            t1.TaxID,
+            t0.Organism_Name,
+            t0.BioProject_Accession,
+            t0.Group,
+            t0.SubGroup,
+            t0.Size,
+            t0.GC,
+            t0.Chromosomes,
+            t0.WGS,
+            t0.Scaffolds,
+            t0.Release_Date,
+            t0.Status
+        FROM   t0, t1
+        WHERE 1 = 1
+        AND t0.BioProject_Accession = t1.Project_Accession
+    };
+
+    #AND t0.Organism_Name = t1.Organism_Name
+    my $header_sth = $dbh->prepare($query);
+    $header_sth->execute;
+    $header_sth->finish;
+
+    # prepare output csv file
+    my $csv = Text::CSV_XS->new( { binary => 1, eol => "\n" } );
+    open my $csv_fh, ">", $strain_file or die "$strain_file: $!";
+    my @cols_name = map { s/^t[01]\.//; $_ } @{ $header_sth->{'NAME'} };
+    $csv->print(
+        $csv_fh,
+        [   @cols_name,
+            qw{ species species_id genus genus_id species_member
+                genus_species_member genus_strain_member }
+        ]
+    );
+
+    my @strs = (
+        q{ AND t0.Status = 'Complete'
+            AND t0.Chromosomes <> '-'
+            AND t0.Chromosomes <> ''
+            ORDER BY t0.Release_Date },    # prok
+        q{ AND t0.Status = 'Chromosomes'
+            AND t0.Chromosomes <> '-'
+            AND t0.Chromosomes <> ''
+            ORDER BY t0.Release_Date },    # euk
+        q{ AND t0.Status = 'Scaffolds or contigs'
+            AND t0.WGS <> '-'
+            AND t0.WGS <> ''
+            ORDER BY t0.Release_Date },
+    );
+    my @taxon_ids;
+    for my $str (@strs) {
+        my $join_sth = $dbh->prepare( $query . $str );
+        $join_sth->execute;
+        while ( my @row = $join_sth->fetchrow_array ) {
+            for my $item (@row) {
+                $item = undef if ( $item eq '-' );
+            }
+
+            # find each strains' species and genus
+            my $taxon_id = $row[0];
+            my $name     = $row[1];
+
+            # dedup, make taxon_id unique
+            next if grep { $_ == $row[0] } @taxon_ids;
+
+            my $bac = $taxon_db->get_taxon( -taxonid => $taxon_id );
+            if ( !$bac ) {
+                warn "Can't find taxon for $name\n";
+                next;
+            }
+
+            my $species = find_ancestor( $bac, 'species' );
+            if ($species) {
+                push @row, ( $species->scientific_name, $species->id );
+            }
+            else {
+                warn "Can't find species for $name\n";
+                next;
+            }
+
+            my $genus = find_ancestor( $bac, 'genus' );
+            if ($genus) {
+                push @row, ( $genus->scientific_name, $genus->id );
+            }
+            else {
+                warn "Can't find genus for $name\n";
+                next;
+            }
+
+            push @row, ( undef, undef, undef );    # member numbers
+
+            # write a line
+            push @taxon_ids, $row[0];
+            $csv->print( $csv_fh, \@row );
+        }
+        $join_sth->finish;
+    }
+    close $csv_fh;
+}
+
+if ( $^O ne "Win32" ) {
+    print "\n";
+    system "wc -l $_"
+        for "$gr_dir/prokaryotes.txt", "$gr_dir/eukaryotes.txt",
+        "$bp_dir/summary.txt", $strain_file;
+}
+
+#----------------------------#
+# Finish
+#----------------------------#
+$stopwatch->end_message;
+exit;
+
+#----------------------------------------------------------#
+# Subroutines
+#----------------------------------------------------------#
+sub find_ancestor {
+    my $taxon = shift;
+    my $rank = shift || 'species';
+
+    return $taxon if $taxon->rank eq $rank;
+
+RANK: while (1) {
+        $taxon = $taxon->ancestor;
+        last RANK unless defined $taxon;
+        return $taxon if $taxon->rank eq $rank;
+    }
+
+    return;
+}
+
+__END__
+
+=head1 NAME
+
+gr_strains.pl
+
+=head1 SYNOPSIS
+
+perl gr_strains.pl
+
+perl gr_strains.pl --euk -o euk_strains.csv
+
+=cut
+
