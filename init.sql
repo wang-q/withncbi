13b23c695c73b7a2771b009be1de3faa12352e05
wangq <wangqiang1997@gmail.com>
2014-04-26 13:30:54 +0800

    Simplified NCBI genome report db schema

---
+++ init.sql
@@ -0,0 +1,30 @@
+create table gr
+(
+    taxonomy_id                     int,
+    organism_name                   char(255),
+    bioproject                      char(255),
+    `group`                         char(255),
+    subgroup                        char(255),
+    genome_size                     double,
+    gc_content                      double,
+    chr                             text,
+    wgs                             char(255),
+    scaffolds                       int,
+    released_date                   date,
+    status                          char(255),
+    code                            char(255),
+    species                         char(255),
+    species_id                      int,
+    genus                           char(255),
+    genus_id                        int,
+    species_member                  int,
+    genus_species_member            int,
+    genus_strain_member             int,
+    primary key (taxonomy_id)
+)
+ENGINE = MyISAM;
+
+create index organism_name_K on gr
+(
+   organism_name
+);
