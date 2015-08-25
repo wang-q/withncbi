fc8e5299b0f1b0390971da48cb3829685c4d5c8d
wang-q <wang-q@outlook.com>
2015-08-26 06:40:26 +0800

    Generate id_seq.csv from refseq plastid file

---
+++ taxon/gb_taxon_locus.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+
+use Path::Tiny;
+
+my $file = shift;
+
+if ( !$file ) {
+    die "You should provide a .gb file.\n";
+}
+elsif ( !-e $file ) {
+    die "[$file] doesn't exist.\n";
+}
+
+$file = path($file);
+
+my $content = $file->slurp;
+
+my @gbs = grep {/\S+/} split( /\/\//, $content );
+
+printf STDERR "There are [%d] sequences.\n", scalar @gbs;
+
+my $count;
+for my $gb (@gbs) {
+    my ( $locus, $taxon );
+
+    if ( $gb =~ /LOCUS\s+(\w+)\s+/ ) {
+        $locus = $1;
+    }
+    else {
+        warn "Can't get locus\n";
+        next;
+    }
+
+    if ( $gb =~ /db_xref\=\"taxon\:(\d+)/ ) {
+        $taxon = $1;
+    }
+    else {
+        warn "Can't get taxon\n";
+        next;
+    }
+
+    printf "%s,%s\n", $taxon, $locus;
+    $count++;
+}
+
+printf STDERR "There are [%d] valid sequences.\n", $count;
+
+__END__
+
+=head1 NAME
+
+gb_taxon_locus.pl - scan a multi-sequence .gb file
+
+=head1 SYNOPSIS
+
+    wget -N ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/organelle/plastid/plastid.1.genomic.gbff.gz
+    gzip -d plastid.1.genomic.gbff.gz
+    
+    perl gb_taxon_locus.pl plastid.1.genomic.gbff > refseq_id_seq.csv
+
+=cut
