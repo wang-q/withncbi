8b192c7b238eae91a046c181c7e5d3fd27482d5b
wang-q <wang-q@outlook.com>
2016-04-11 04:24:07 +0800

    From stdin

--- taxon/merge_csv.pl
+++ taxon/merge_csv.pl
@@ -3,155 +3,114 @@ use strict;
 use warnings;
 use autodie;
 
-use Getopt::Long;
-use Pod::Usage;
-use Config::Tiny;
+use Getopt::Long qw(HelpMessage);
+use FindBin;
 use YAML qw(Dump Load DumpFile LoadFile);
 
 use List::MoreUtils qw(firstidx);
+use Path::Tiny;
+use Set::Scalar;
 
 #----------------------------------------------------------#
 # GetOpt section
 #----------------------------------------------------------#
-my $file_target;
-my $file_merge;
 
-my @fields;
-my @fields2;
+=head1 NAME
 
-my $concat;
-my $stdout;
+merge_csv.pl - Merge csv files based on @fields
 
-my $man  = 0;
-my $help = 0;
+=head1 SYNOPSIS
+
+    cat input.csv another.csv | perl merge_csv.pl [options]
+      Options:
+        --help      -?          brief help message
+        --outfile   -o  STR     output filename. Default is [stdout] for screen
+        --fields    -f  @INT    fields as identifies. Default is [0], first column
+        --concat                do concat other than merge. Keep first ID fields
+
+    cat 1.csv 2.csv | perl merge_csv.pl -f 0 -f 1
+
+=cut
 
 GetOptions(
-    'help|?'     => \$help,
-    'man'        => \$man,
-    't|ft=s'     => \$file_target,
-    'm|fm=s'     => \$file_merge,
-    'f|fields=s' => \@fields,
-    'f2|fields2=s' => \@fields2,
-    'concat'     => \$concat,
-    'stdout'     => \$stdout,
-) or pod2usage(2);
-
-pod2usage(1) if $help;
-pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
-
-for ( $file_target, $file_merge ) {
-    die "Can't find file [$_]\n" unless -e $_;
-}
+    'help|?' => sub { HelpMessage(0) },
+    'outfile|o=s' => \( my $outfile = 'stdout' ),
+    'fields|f=s'  => \my @fields,
+    'concat'      => \my $concat,
+) or HelpMessage(1);
 
 if ( !scalar @fields ) {
     @fields = (0);
 }
+@fields = sort @fields;    # make array splicing happier
 
 #----------------------------------------------------------#
 # apply
 #----------------------------------------------------------#
-my ( $count_t, $count_m ) = ( 0, 0 );
+my ( $count_all, $count_op ) = ( 0, 0 );
 
-# read in merge
-my $id_m   = [];
-my $line_m = [];
-{
-    open my $fh, '<', $file_merge;
-    while (<$fh>) {
-        chomp;
-        next unless $_;
-        my $id;
-        if ($concat and scalar @fields2) {
-            $id = join( "_", ( split /,/ )[@fields2] );
+my $id_set = Set::Scalar->new;
+my ( @ids, @lines );
+
+while ( my $line = <> ) {
+    chomp $line;
+    next unless $line;
+    $count_all++;
+    my $id = join( "_", ( split ",", $line )[@fields] );
+    if ( $id_set->has($id) ) {
+        $count_op++;
+        if ($concat) {
+            my $index = firstidx { $_ eq $id } @ids;
+            my $ori_line = $lines[$index];
+
+            my @fs = split ",", $line;
+            for my $f_idx ( reverse @fields ) {
+                splice @fs, $f_idx, 1;
+            }
+            $lines[$index] = join ",", $ori_line, @fs;
         }
-        else {
-            $id = join( "_", ( split /,/ )[@fields] );
-        }
-        push @{$id_m}, $id;
-        push @{$line_m}, $_;
-        $count_m++;
     }
-    close $fh;
+    else {
+        $id_set->insert($id);
+        push @ids,   $id;
+        push @lines, $line;
+    }
 }
 
-# read in target
-my $line_t = [];
+#----------------------------#
+# check
+#----------------------------#
 {
-    open my $fh, '<', $file_target;
-    while (<$fh>) {
-        chomp;
-        next unless $_;
-        my $id = join( "_", ( split /,/ )[@fields] );
-        if ( grep { $_ eq $id } @{$id_m} ) {
-            $count_t++;
-            my $idx = firstidx { $_ eq $id } @{$id_m};
-            splice @{$id_m}, $idx, 1;
-            my ($line) = splice @{$line_m}, $idx, 1;
-
-            my $newline;
-            if ($concat) {
-                $newline = "$_,$line";
-            }
-            else {
-                $newline = $line;
-            }
-            push @{$line_t}, $newline;
-        }
-        else {
-            push @{$line_t}, $_;
-        }
+    my %seen;
+    for (@lines) {
+        my $number = scalar split(",");
+        $seen{$number}++;
     }
-    close $fh;
+    if ( keys(%seen) > 1 ) {
+        warn "*** Fields not identical, be careful.\n";
+        warn Dump { fields => \%seen, };
+    }
 }
 
 #----------------------------#
 # write outputs
 #----------------------------#
-if ( !$concat ) {
-    push @{$line_t}, @{$line_m};
-}
-
-if ($stdout) {
-    for ( @{$line_t} ) {
-        print $_, "\n";
-    }
+my $out_fh;
+if ( lc($outfile) eq "stdout" ) {
+    $out_fh = *STDOUT;
 }
 else {
-    open my $fh, '>', $file_target;
-    for ( @{$line_t} ) {
-        print {$fh} $_, "\n";
-    }
-    close $fh;
+    open $out_fh, ">", $outfile;
 }
 
-printf STDERR "%s [%s] lines from %s\n", $concat ? "Concat" : "Remove",
-    $count_t,
-    $file_target;
-printf STDERR "Add [%s] lines from %s\n", $count_m, $file_merge;
+for (@lines) {
+    print {$out_fh} $_ . "\n";
+}
+close $out_fh;
+
+printf STDERR "Total lines [%d]; Result lines [%d]. ", $count_all, scalar @lines;
+printf STDERR "%s [%d] lines.\n", $concat ? "Concat" : "Merge", $count_op;
 
 exit;
 
 __END__
-
-
-=head1 NAME
-
-merge_csv.pl - Merge two csv files based on @fields
-
-=head1 SYNOPSIS
-
-    merge_csv.pl [options]
-      Options:
-        --help                  brief help message
-        --man                   full documentation
-        -t, --ft STR            target file (output)
-        -m, --fm SRT            merge file
-        -f, --fields @INT       fields as identifies
-        --concat                do concat other than merge
-        -f2, --fieldss @INT     fields in concat file
-        --stdout                write outputs to stdout instead of the target file
-
-    perl merge_csv.pl -t ../init/taxon.csv -m d:\data\alignment\yeast_combine\taxon.csv 
-    perl merge_csv.pl -t ../init/chr_length.csv -m d:\data\alignment\yeast_combine\chr_length.csv
-
-=cut
