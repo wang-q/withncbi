633b55822f1ffcca0370460bf7b8d79e4c1de5c9
wang-q <wang-q@outlook.com>
2016-07-11 20:31:16 +0800

    Prompts to stderr

--- util/shuffle.sh
+++ util/shuffle.sh
@@ -5,7 +5,7 @@ USAGE="Usage: $0 FASTA_FILE SEQ_NUMBER MINIMAL_LENGTH MAXIMAL_LENGTH"
 # bash shuffle.sh ~/data/rna-seq/medfood/process/Arabidopsis_thaliana/result/Trinity.fasta 100 1000 2000
 
 if [ "$#" -lt 1 ]; then
-    echo "$USAGE"
+    echo >&2 "$USAGE"
     exit 1
 fi
 
@@ -22,13 +22,13 @@ SEQ_NUMBER=${2:-100}
 MINIMAL_LENGTH=${3:-1000}
 MAXIMAL_LENGTH=${4:-2000}
 
-echo "==> Parameters <=="
-echo "    FASTA_FILE=${FASTA_FILE}"
-echo "    SEQ_NUMBER=${SEQ_NUMBER}"
-echo "    MINIMAL_LENGTH=${MINIMAL_LENGTH}"
-echo "    MAXIMAL_LENGTH=${MAXIMAL_LENGTH}"
+echo >&2 "==> Parameters <=="
+echo >&2 "    FASTA_FILE=${FASTA_FILE}"
+echo >&2 "    SEQ_NUMBER=${SEQ_NUMBER}"
+echo >&2 "    MINIMAL_LENGTH=${MINIMAL_LENGTH}"
+echo >&2 "    MAXIMAL_LENGTH=${MAXIMAL_LENGTH}"
 
-echo "==> Getting list of sequences"
+echo >&2 "==> Getting list of sequences"
 faops filter -a ${MINIMAL_LENGTH} -z ${MAXIMAL_LENGTH} ${FASTA_FILE} stdout \
     | perl -nl -e '/^>(\S+)/ or next; print $1' \
     | perl -MList::Util=shuffle -wne 'print shuffle <>;' \
@@ -38,5 +38,5 @@ faops filter -a ${MINIMAL_LENGTH} -z ${MAXIMAL_LENGTH} ${FASTA_FILE} stdout \
 
 [ $? -ne 0 ] && echo >&2 "Getting list of sequences failed" && exit 255
 
-echo "==> Getting sequences from list"
+echo >&2 "==> Getting sequences from list"
 faops some ${FASTA_FILE} seq_list.tmp stdout
