d3b00a10c0f00106b8aa698a7feefeb8f52edbc1
wang-q <wang-q@outlook.com>
2018-04-18 07:32:29 +0800

    assembly_prep.pl - prepare ASSEMBLY materials

--- taxon/assembly_prep.pl
+++ taxon/assembly_prep.pl
@@ -35,10 +35,11 @@ assembly_prep.pl - prepare ASSEMBLY materials
 
     #name   ftp_path    organism    assembly_level
 
-    Three files will be generated.
-    trichoderma.csv
-    trichoderma.url.txt
-    trichoderma.data.txt
+    Two files will be generated.
+    trichoderma.assembly.rsync.sh
+    trichoderma.assembly.collect.sh
+
+    The latter one will create trichoderma.assembly.collect.csv
 
 =cut
 
@@ -74,172 +75,136 @@ my @orig_orders;
     }
 }
 
-#----------------------------#
-# rsync .sh
-#----------------------------#
-$stopwatch->block_message("Generate rsync .sh for downloading ");
-
 Path::Tiny::path($outdir)->mkpath();
-my $file_sh = Path::Tiny::path( $outdir, "$basename.sh" );
-$file_sh->remove if $file_sh->is_file;
 
-$file_sh->append(
-    <<'EOF'
+#----------------------------#
+# rsync
+#----------------------------#
+{
+    $stopwatch->block_message("Generate .rsync.sh");
+
+    my $file_rsync = Path::Tiny::path( $outdir, "$basename.rsync.sh" );
+    $file_rsync->remove if $file_rsync->is_file;
+
+    $file_rsync->append(
+        <<'EOF'
 #!/bin/bash
 
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${BASE_DIR}
 
 EOF
-);
+    );
 
-for my $key (@orig_orders) {
-    my $ftp = $ftp_of->{$key};
+    for my $key (@orig_orders) {
+        my $ftp = $ftp_of->{$key};
 
-    # ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/167/675/GCA_000167675.2_v2.0
-    # ftp.ncbi.nlm.nih.gov::genomes/all/GCA/000/167/675/GCA_000167675.2_v2.0
+        # ftp   - ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/167/675/GCA_000167675.2_v2.0
+        # rsync - ftp.ncbi.nlm.nih.gov::genomes/all/GCA/000/167/675/GCA_000167675.2_v2.0
 
-    my $rsync = $ftp;
-    $rsync =~ s/ftp:\/\/ftp.ncbi.nlm.nih.gov\//ftp.ncbi.nlm.nih.gov::/;
-    if ( $rsync eq $ftp ) {
-        die "Check the ftp url: $ftp\n";
-    }
+        my $rsync = $ftp;
+        $rsync =~ s/ftp:\/\/ftp.ncbi.nlm.nih.gov\//ftp.ncbi.nlm.nih.gov::/;
+        if ( $rsync eq $ftp ) {
+            die "Check the ftp url: [$key] $ftp\n";
+        }
 
-    $file_sh->append(
-        <<"EOF"
+        $file_rsync->append(
+            <<"EOF"
 echo >&2
 echo >&2 "==> $key"
 mkdir -p $key
 rsync -avP $rsync/ $key/
 
 EOF
-    );
+        );
+    }
 }
 
-#{
-#    mkdir $outdir unless -d $outdir;
-#
-#    my $csv = Text::CSV_XS->new( { binary => 1 } )
-#        or die "Cannot use CSV: " . Text::CSV_XS->error_diag;
-#    $csv->eol("\n");
-#
-#    my $file_csv = Path::Tiny::path( $outdir, "$basename.csv" )->stringify;
-#
-#    open my $csv_fh, ">", $file_csv;
-#
-#    my @columns = (
-#        'prefix',                'taxon_id',     'name',         'Organism',
-#        'Biosource',             'BioProject',   'Keywords',     'Genome_Coverage',
-#        'Sequencing_Technology', '#_of_Contigs', 'Total_length', 'Assembly_Method',
-#        'Assembly_Name',         'Update_date',  'pubmed',
-#    );
-#
-#    $csv->print( $csv_fh, \@columns );
-#
-#    for my $key (@orig_orders) {
-#
-#        # Don't use hashref here, because I want use hash slices.
-#        my %info = %{ $master->{$key} };
-#
-#        if ( !$csvonly and $fix_strain ) {
-#            if ( grep { $_ eq $key } @nofix ) {
-#                print "Skip $info{name} as you don't want fix it\n";
-#            }
-#            elsif ( $info{Organism} =~ /$info{Biosource}/ ) {
-#                print "Don't need fixing for $info{name}\n";
-#            }
-#            else {
-#                # Sometimes the uploader didn't create a new strain, assign its own id and marked
-#                # species name as strain name.
-#                # So try looking up this strain in taxonomy dumps
-#                print "Fix strain taxon info for $info{name}\n";
-#                $info{Organism} = $info{Organism} . " " . $info{Biosource};
-#
-#                my $node;
-#                eval { $node = $taxon_db->get_taxon( -name => $info{Organism} ); };
-#                if ( $@ or !$node ) {
-#                    print " " x 4, "Can't find taxon for $info{name}\n";
-#                    $arbitrary++;
-#                    print " " x 4, "Give it arbitrary id as $arbitrary\n";
-#                    $info{original_id} = $info{taxon_id};
-#                    $info{taxon_id}    = $arbitrary;
-#                }
-#                else {
-#                    $info{taxon_id} = $node->id;
-#                }
-#            }
-#        }
-#        $csv->print( $csv_fh, [ @info{@columns} ] );
-#        $master->{$key} = \%info;
-#
-#    }
-#    close $csv_fh;
-#
-#    print "\n", "=" x 20, "\n";
-#    print ".csv generated.\n";
-#
-#    if ( !$csvonly ) {
-#        my $file_url = Path::Tiny::path( $outdir, "$basename.url.txt" )->stringify;
-#        open my $url_fh, ">", $file_url;
-#        for my $key (@orig_orders) {
-#
-#            my %info = %{ $master->{$key} };
-#
-#            my @downloads = @{ $info{download} };
-#            if ($aria2) {
-#                for (@downloads) {
-#                    print {$url_fh} $_, "\n";
-#                    print {$url_fh} "  dir=", "$outdir/$key", "\n";
-#                }
-#            }
-#            else {
-#                print {$url_fh} $_, "\n" for @downloads;
-#            }
-#
-#        }
-#        close $url_fh;
-#        if ($aria2) {
-#            print "# Run the follow cmd to download with aria2c\n";
-#            print "aria2c -UWget -x 6 -s 3 -c -i $file_url\n";
-#            print "\n";
-#            print "# Use the following cmd to check .gz files\n";
-#            print "find $outdir -name \"*.gz\" | xargs gzip -t \n";
-#        }
-#        else {
-#            print "Downloading urls in file $file_url\n";
-#        }
-#    }
-#}
+#----------------------------#
+# collect
+#----------------------------#
+{
+    $stopwatch->block_message("Generate .collect.sh");
 
-#----------------------------#
-# @data yaml
-#----------------------------#
-#if ( !$csvonly ) {
-#    $stopwatch->block_message("Generate .data.yml");
-#
-#    my $file_data = Path::Tiny::path( $outdir, "$basename.data.yml" )->stringify;
-#
-#    my $text = <<'EOF';
-#---
-#data:
-#[% FOREACH name IN names -%]
-#  - taxon: [% master.$name.taxon_id %]
-#    name: [% master.$name.name %]
-#    sciname: [% master.$name.Organism %]
-#    prefix: [% master.$name.prefix %]
-#    coverage: [% master.$name.Genome_Coverage %] [% master.$name.Sequencing_Technology %]
-#[% IF master.$name.original_id -%]
-#    original_id: [% master.$name.original_id %]
-#[% END -%]
-#[% END -%]
-#
-#EOF
-#    my $tt = Template->new;
-#    $tt->process( \$text, { names => [@orig_orders], master => $master, }, $file_data )
-#        or die Template->error;
-#
-#    print ".data.yml generated.\n";
-#}
+    my $file_collect = Path::Tiny::path( $outdir, "$basename.collect.sh" )->stringify;
+
+    my @columns = (
+        "Organism name",
+        "Taxid",
+        "Assembly name",
+        "Infraspecific name",
+        "BioSample",
+        "BioProject",
+        "Submitter",
+        "Date",
+        "Assembly type",
+        "Release type",
+        "Assembly level",
+        "Genome representation",
+        "WGS project",
+        "Assembly method",
+        "Genome coverage",
+        "Sequencing technology",
+        "RefSeq category",
+        "RefSeq assembly accession",
+        "GenBank assembly accession",
+    );
+
+    my $template = <<'EOF';
+#!/bin/bash
+
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${BASE_DIR}
+
+echo "[% header %]" \
+    > [% basename %].collect.csv
+
+[% FOREACH n IN names -%]
+echo >&2
+echo >&2 "==> [% n %]"
+find [% n %] -type f -name "*_assembly_report.txt" |
+    xargs cat |
+    perl -nl -e '
+        BEGIN { our %stat = (); }
+
+        m{^#\s+} or next;
+        s/^#\s+//;
+        @O = split /\:\s*/;
+        scalar @O == 2 or next;
+        $O[0] =~ s/\s*$//g;
+        $O[0] =~ s/\W/_/g;
+        $O[1] =~ /([\w .-]+)/ or next;
+        $stat{$O[0]} = $1;
+
+        END {
+            my @c;
+            for my $key ( qw{ [% columns %] } ) {
+                if (exists $stat{$key}) {
+                    push @c, $stat{$key};
+                }
+                else {
+                    push @c, q{};
+                }
+            }
+            print join(q{,}, @c);
+        }
+    ' \
+    >> [% basename %].collect.csv
+
+[% END -%]
+
+EOF
+    my $tt = Template->new;
+    $tt->process(
+        \$template,
+        {   basename => $basename,
+            names    => \@orig_orders,
+            header   => join( ",", map {s/\s+/_/g; $_} @columns ),
+            columns  => join( " ", map {s/\s+/_/g; $_} @columns ),
+        },
+        $file_collect
+    ) or die Template->error;
+}
 
 $stopwatch->end_message;
 
